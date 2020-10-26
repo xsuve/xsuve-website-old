f273f3ca0b1761c2022be365675880c56c1e103b
George Babă <xsuve@Georges-MacBook-Pro.local>
2020-10-27 00:19:34 +0200

    Delete demo project

--- demo/luxx/includes/modules/expenses/views/_templates/header.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" type="text/css" href="<?php echo URL; ?>includes/modules/expenses/public/css/style.css">
-<script type="text/javascript" src="<?php echo URL; ?>includes/modules/expenses/public/js/script.js"></script>
